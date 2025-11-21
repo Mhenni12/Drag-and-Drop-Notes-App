5f4a2fc66e72afe49ed2a066a1ff907c31e1ddff
Mhenni12 <medazizmhenni12@gmail.com>
2025-11-21 13:27:43 +0100

    feat: enhance NoteCard component with styling and add Trash icon

--- NotesApp/src/components/NoteCard.jsx
+++ NotesApp/src/components/NoteCard.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React from "react";
+import Trash from "../icons/Trash";
 
-const NoteCard = ({note}) => {
+const NoteCard = ({ note }) => {
   const body = JSON.parse(note.body);
+  const position = JSON.parse(note.position);
+  const colors = JSON.parse(note.colors);
   return (
-    <div>
-      {body}
-    </div>
-  )
-}
+    <div className="card" style={{ backgroundColor: colors.colorBody }}>
+      <div
+        className="card-header"
+        style={{ backgroundColor: colors.colorHeader }}
+      >
+        <Trash />
+      </div>
 
-export default NoteCard
+      <div className="card-body">
+        <textarea
+          defaultValue={body}
+          style={{ color: colors.colorText }}
+        ></textarea>
+      </div>
+    </div>
+  );
+};
+
+export default NoteCard;
