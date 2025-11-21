d70cf5695386e635230a2993f988471f5ed7e402
Mhenni12 <medazizmhenni12@gmail.com>
2025-11-21 13:45:26 +0100

    feat: implement auto-grow functionality for textarea in NoteCard component and adjust the position of the cards

--- NotesApp/src/components/NoteCard.jsx
+++ NotesApp/src/components/NoteCard.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import { useRef, useEffect } from "react";
 import Trash from "../icons/Trash";
 
 const NoteCard = ({ note }) => {
   const body = JSON.parse(note.body);
   const position = JSON.parse(note.position);
   const colors = JSON.parse(note.colors);
+
+  const textAreaRef = useRef(null);
+
+  // Auto-grow effect for the textarea to get rid of scrollbar
+  function autoGrow(textAreaRef) {
+    const { current } = textAreaRef;
+    current.style.height = "auto"; // Reset the height
+    current.style.height = current.scrollHeight + "px"; // Set the new height
+  }
+
+  // Adjust textarea height on component mount
+  useEffect(() => {
+    autoGrow(textAreaRef);
+  }, []);
+
   return (
-    <div className="card" style={{ backgroundColor: colors.colorBody }}>
+    <div
+      className="card"
+      style={{
+        backgroundColor: colors.colorBody,
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      }}
+    >
       <div
         className="card-header"
         style={{ backgroundColor: colors.colorHeader }}
@@ -16,8 +38,12 @@ const NoteCard = ({ note }) => {
 
       <div className="card-body">
         <textarea
+          ref={textAreaRef}
           defaultValue={body}
           style={{ color: colors.colorText }}
+          onInput={() => {
+            autoGrow(textAreaRef);
+          }} // As we're typing, adjust height
         ></textarea>
       </div>
     </div>
