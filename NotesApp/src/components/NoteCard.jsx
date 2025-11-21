da4e2132bfdd77aaef405291e60f9a37537e994c
Mhenni12 <medazizmhenni12@gmail.com>
2025-11-21 14:07:51 +0100

    feat: implement drag-and-drop functionality in NoteCard component

--- NotesApp/src/components/NoteCard.jsx
+++ NotesApp/src/components/NoteCard.jsx
@@ -1,12 +1,17 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import Trash from "../icons/Trash";
 
 const NoteCard = ({ note }) => {
   const body = JSON.parse(note.body);
-  const position = JSON.parse(note.position);
+  // Use state to manage position for drag-and-drop functionality
+  const [position, setPositon] = useState(JSON.parse(note.position));
   const colors = JSON.parse(note.colors);
 
   const textAreaRef = useRef(null);
+
+  const mouseStartPos = useRef({ x: 0, y: 0 });
+
+  const cardRef = useRef(null);
 
   // Auto-grow effect for the textarea to get rid of scrollbar
   function autoGrow(textAreaRef) {
@@ -20,8 +25,43 @@ const NoteCard = ({ note }) => {
     autoGrow(textAreaRef);
   }, []);
 
+  // Mouse down event to initiate dragging
+  // Capture the starting x & y position
+  // Listen for the following mousemove events.
+  const mouseDown = (e) => {
+    mouseStartPos.current.x = e.clientX;
+    mouseStartPos.current.y = e.clientY;
+
+    document.addEventListener("mousemove", mouseMove);
+    document.addEventListener("mouseup", mouseUp);
+  };
+
+  const mouseMove = (e) => {
+    //1 - Calculate move direction
+    let mouseMoveDir = {
+      x: mouseStartPos.current.x - e.clientX,
+      y: mouseStartPos.current.y - e.clientY,
+    };
+
+    //2 - Update start position for next move.
+    mouseStartPos.current.x = e.clientX;
+    mouseStartPos.current.y = e.clientY;
+
+    //3 - Update card top and left position.
+    setPositon({
+      x: cardRef.current.offsetLeft - mouseMoveDir.x,
+      y: cardRef.current.offsetTop - mouseMoveDir.y,
+    });
+  };
+
+  const mouseUp = () => {
+    document.removeEventListener("mousemove", mouseMove);
+    document.removeEventListener("mouseup", mouseUp);
+  };
+
   return (
     <div
+      ref={cardRef}
       className="card"
       style={{
         backgroundColor: colors.colorBody,
@@ -32,6 +72,7 @@ const NoteCard = ({ note }) => {
       <div
         className="card-header"
         style={{ backgroundColor: colors.colorHeader }}
+        onMouseDown={mouseDown}
       >
         <Trash />
       </div>
